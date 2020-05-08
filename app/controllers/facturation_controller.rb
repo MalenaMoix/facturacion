93f01c25b33009171e3afb7659ee46f23175dad3
MalenaMoix <moixmalena@gmail.com>
2020-05-08 17:19:28 -0300

    Understanding queries

--- app/controllers/facturation_controller.rb
+++ app/controllers/facturation_controller.rb
@@ -1,7 +1,14 @@
 class FacturationController < ApplicationController
 
     def index
-        #@project = Project.find(params[:project_id])
+        @project = Project.find(params[:project_id])
+        @users = User.all
+        @members = Member.where(:project_id => @project[:id])
         @facturations = Facturation.all
+
+
+        @current = User.current
+        month_to_show = DateTime.new(DateTime.now.year, DateTime.now.month, 1)
+        @time_entries = TimeEntry.where(:tmonth => month_to_show.month, :tyear => month_to_show.year, :project_id => @project[:id])
     end
 end
