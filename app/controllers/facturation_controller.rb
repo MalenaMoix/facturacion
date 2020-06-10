30b2bbc90214c12502f294ff53af26acb934c286
MalenaMoix <moixmalena@gmail.com>
2020-06-10 11:01:32 -0300

    Arreglando un syntax error y actualizacion del todo.txt

--- app/controllers/facturation_controller.rb
+++ app/controllers/facturation_controller.rb
@@ -1,15 +1,15 @@
 class FacturationController < ApplicationController
 
     def index
-        @project = Project.find(params[:project_id])
-        @facturations = Facturation.all
-        @empleados_facturation = ProjectAssignedUser.where(:project_id => @project[:id])
+      @project = Project.find(params[:project_id])
+      @facturations = Facturation.all
+      @empleados_facturation = ProjectAssignedUser.where(:project_id => @project[:id])
        
 
-        #@current = User.current
-        month_to_show = DateTime.new(DateTime.now.year, DateTime.now.month, 1)
-        @time_entries = TimeEntry.where(:tmonth => month_to_show.month, :tyear => month_to_show.year, :project_id => @project[:id])
-        @months = TimeEntry.select('tyear, tmonth').where(:project_id => @project[:id]).group('tyear, tmonth')
+      #@current = User.current
+      month_to_show = DateTime.new(DateTime.now.year, DateTime.now.month, 1)
+      @time_entries = TimeEntry.where(:tmonth => month_to_show.month, :tyear => month_to_show.year, :project_id => @project[:id])
+      @months = TimeEntry.select('tyear, tmonth').where(:project_id => @project[:id]).group('tyear, tmonth')
     end
 
 
