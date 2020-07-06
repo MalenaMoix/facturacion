d7668c4bd160379fc4a17e2f3b7d030a51c40d75
cristiaan3003 <cristiaan3003@gmail.com>
2020-07-06 09:24:35 -0300

    bloquear factura

--- app/controllers/facturation_controller.rb
+++ app/controllers/facturation_controller.rb
@@ -48,20 +48,27 @@ class FacturationController < ApplicationController
         total_bill = row[:input_total_bill]
         log_comment = row[:log_comment]
         user_name_modification = User.current.lastname + ' ' + User.current.firstname
-        last_date_modification = Date.current
+        #last_date_modification = Date.current
         input_concepts = row[:input_concepts]
         input_concepts_monto = row[:input_concepts_monto]
         facturation_comments = row[:facturation_comments]
         checkbox_editar = row[:checkbox_editar]
-
+        checkbox_bloqueado = row[:checkbox_bloqueado]
 
         employee_to_update = ProjectAssignedUser.where(:project_id => project_id, :user_id => user_id).first
         
+        #if not equal -> checkbox state it was change for the user
+        # so I save this change in state and date of this change.
+        if checkbox_bloqueado != employee_to_update.bloqueado
+          employee_to_update.bloqueado = checkbox_bloqueado
+          employee_to_update.last_modification_date_bloqueado = Date.current
+          employee_to_update.user_name_last_mod = user_name_modification
+        end
         if checkbox_editar
           employee_to_update.total = total_bill
           employee_to_update.log = log_comment
           employee_to_update.user_name_modification = user_name_modification
-          employee_to_update.last_date_modification = last_date_modification
+          employee_to_update.last_date_modification = Date.current
         end
 
         employee_to_update.other_amounts_comments = input_concepts
